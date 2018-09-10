9d958fa7d57f93bae400e0adb724eb8aa0d1d77e
xiaoxinga <xiaoxinga@yonyou.com>
2018-09-10 14:38:32 +0800

    文文网

--- src/main/java/persons/controller/PersonController.java
+++ src/main/java/persons/controller/PersonController.java
@@ -1,13 +1,28 @@
 package persons.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import persons.dao.PersonDao;
 import persons.mapper.PersonMapper;
+
+import java.util.Map;
+
 /**
  * @author xiaoxinga
  * @date 2018/9/10 11:53
  * @since
  */
+@RestController
+@RequestMapping(path = "/person")
 public class PersonController {
     @Autowired
-    PersonMapper syncSellerMobileService;
+    PersonMapper personMapper;
+
+    @RequestMapping(path = "/addPerson", method = RequestMethod.POST)
+    public void addPerson(@RequestBody Map<String, String> param) throws Exception {
+
+    }
 }
