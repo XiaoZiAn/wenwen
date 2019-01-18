13f8f794f6904487bf463999d920dc958aefa904
xiaoxinga <xiaoxinga@yonyou.com>
2019-01-18 11:48:44 +0800

    优化

--- src/main/java/com/wenwen/system/service/EmailTemplateService.java
+++ src/main/java/com/wenwen/system/service/EmailTemplateService.java
@@ -3,12 +3,14 @@ package com.wenwen.system.service;
 import com.wenwen.system.dao.EmailTemplate;
 import com.wenwen.system.mapper.EmailTemplateMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author xiaoxinga
  * @date 2019/01/18 10:46
  * @since
  */
+@Service
 public class EmailTemplateService {
 
     @Autowired
